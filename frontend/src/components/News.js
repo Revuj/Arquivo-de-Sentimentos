6273a56df90b7ed25cb48effb3f7553cd28a2544
Revuj <revuj112@gmail.com>
2021-05-01 20:40:18 +0100

    news button showing only when there are news

--- frontend/src/components/News.js
+++ frontend/src/components/News.js
@@ -8,33 +8,35 @@ const News = ({ t, previews }) => {
   const [hidden, setHidden] = useState(true);
 
   return (
-    <>
-      <div id="news-column-container" className={hidden ? 'hidden' : ''}>
-        <div id="news-column">
-          <ul id="news-list">
-            {previews &&
-              [...previews.previews].map((preview) => {
-                return (
-                  <li className="news-item">
-                    <span className="news-source">{preview.site_name}</span>
-                    <h5 className="news-title">{preview.title}</h5>
-                    <p className="news-description">{preview.description}</p>
-                    <img className="news-image" src={preview.image} />
-                  </li>
-                );
-              })}
-          </ul>
+    previews && (
+      <>
+        <div id="news-column-container" className={hidden ? 'hidden' : ''}>
+          <div id="news-column">
+            <ul id="news-list">
+              {previews &&
+                [...previews.previews].map((preview) => {
+                  return (
+                    <li className="news-item">
+                      <span className="news-source">{preview.site_name}</span>
+                      <h5 className="news-title">{preview.title}</h5>
+                      <p className="news-description">{preview.description}</p>
+                      <img className="news-image" src={preview.image} />
+                    </li>
+                  );
+                })}
+            </ul>
+          </div>
         </div>
-      </div>
-      <Button
-        type="button"
-        className={!hidden ? 'open example-button' : 'example-button'}
-        id="show-news-button"
-        onClick={() => setHidden(!hidden)}
-      >
-        <ImNewspaper size={30} />
-      </Button>
-    </>
+        <Button
+          type="button"
+          className={!hidden ? 'open example-button' : 'example-button'}
+          id="show-news-button"
+          onClick={() => setHidden(!hidden)}
+        >
+          <ImNewspaper size={30} />
+        </Button>
+      </>
+    )
   );
 };
 
