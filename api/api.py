0a976d990448806831baf8df9985aca82ec01945
Revuj <revuj112@gmail.com>
2021-05-03 15:43:44 +0100

    added cors and root endpoint

--- api/api.py
+++ api/api.py
@@ -1,21 +1,21 @@
 from dotenv import load_dotenv
 load_dotenv()
-from flask import Flask, request
+from flask import Flask, request, jsonify, make_response
+from flask_cors import CORS, cross_origin
 from mongo import mongo_client
+import boto3
 import analysis
 import os
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder='frontend/build')
+cors = CORS(app)
 
 sources_urls = {'Correio da Manhã': 'www.cmjornal.pt', 'Jornal de Notícias': 'www.jn.pt', 'Público': 'www.publico.pt'}
 
 @app.route('/analyse', methods=['POST', 'GET'])
+@cross_origin()
 def analyse():
-    '''
-    db = mongo_client.ArquivoSentimentos
-    db.ArquivoSentimentos.update_many({}, {'$unset': {'link_previews':1}})
-    '''
     entity = request.args.get('entity')
     source = request.args.get('source')
 
@@ -25,12 +25,16 @@ def analyse():
 
 
 @app.route('/previews')
+@cross_origin()
 def previews():
-
     entity = request.args.get('entity')
     source = request.args.get('source')
     return {'previews': analysis.get_link_previews(entity, source)}
 
 
+@app.route('/')
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
+    
 if __name__ == '__main__':
     app.run(threaded=True)
