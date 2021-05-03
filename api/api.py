c0ed4b28b1427af15e55d60f64ebfa2515f9eea2
Revuj <revuj112@gmail.com>
2021-05-03 17:42:09 +0100

    new build

--- api/api.py
+++ api/api.py
@@ -8,12 +8,10 @@ import os
 
 
 app = Flask(__name__, static_url_path='', static_folder='frontend/build')
-cors = CORS(app)
 
 sources_urls = {'Correio da Manhã': 'www.cmjornal.pt', 'Jornal de Notícias': 'www.jn.pt', 'Público': 'www.publico.pt'}
 
 @app.route('/analyse', methods=['POST', 'GET'])
-@cross_origin()
 def analyse():
     entity = request.args.get('entity')
     source = request.args.get('source')
@@ -24,16 +22,15 @@ def analyse():
 
 
 @app.route('/previews')
-@cross_origin()
 def previews():
     entity = request.args.get('entity')
     source = request.args.get('source')
     return {'previews': analysis.get_link_previews(entity, source)}
 
 
-@app.route('/', defaults={'path':''})
+@app.route('/')
 def serve():
-    return send_from_directory(app.static_folder, 'index.html')
+    return app.send_static_file('index.html')
 
 if __name__ == '__main__':
     app.run(threaded=True)
