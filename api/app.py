5704c3e7ed0bbcd5488b404f9d54a749cd60e4a3
Revuj <revuj112@gmail.com>
2021-05-05 19:26:55 +0100

    added route that returns cached entities' names

--- api/app.py
+++ api/app.py
@@ -12,6 +12,11 @@ cors = CORS(app)
 
 sources_urls = {'Correio da Manhã': 'www.cmjornal.pt', 'Jornal de Notícias': 'www.jn.pt', 'Público': 'www.publico.pt'}
 
+@app.route('/cache', methods=['GET'])
+@cross_origin()
+def cache():
+    return { 'names': analysis.get_cached_names()}
+
 @app.route('/results')
 @cross_origin()
 def results():
@@ -27,15 +32,12 @@ def analyse():
     analysis_by_year, magnitude_by_year = analysis.analysis(entity, source)
     return { 'sentiment': { source : analysis_by_year }, 'magnitude': { source : magnitude_by_year } }
 
-
-
 @app.route('/previews')
 @cross_origin()
 def previews():
     entity = request.args.get('entity')
     source = request.args.get('source')
     return {'previews': analysis.get_link_previews(entity, source)}
-
 
 @app.route('/')
 def serve():
